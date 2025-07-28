ceec020bde574e6eef6cb3498965876752d0fa48
ramez101 <ramez.werfelli9@gmail.com>
2025-07-28 09:50:50 +0100

    commit

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ import json
 import re
 import unicodedata
 from fuzzywuzzy import fuzz
+import os
+
 
 app = Flask(__name__)
 CORS(app)
@@ -253,4 +255,5 @@ def get_brands():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # use Render's PORT or default to 5000
+    app.run(host="0.0.0.0", port=port, debug=False)
